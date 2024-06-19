da008851a5d4169784ad847dcda4494f166c7e75
prashantchaubey <p.chaubey@softwaremathematics.com>
2024-06-19 18:10:09 +0530

    first commit

--- lib/local_notifications.dart
+++ lib/local_notifications.dart
@@ -13,6 +13,7 @@ class LocalNotifications {
     onClickNotification.add(notificationResponse.payload!);
   }
 
+
 // initialize the local notifications
   static Future init() async {
     // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
